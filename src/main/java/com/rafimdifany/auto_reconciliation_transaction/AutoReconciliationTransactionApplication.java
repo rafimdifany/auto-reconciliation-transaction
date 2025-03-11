c5ad69a4f974c883315092eb7d73421bf7796c50
rafimdifany <rafimdifany@gmail.com>
2025-03-11 21:40:14 +0700

    initial commit 2

---
+++ src/main/java/com/rafimdifany/auto_reconciliation_transaction/AutoReconciliationTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.rafimdifany.auto_reconciliation_transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoReconciliationTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoReconciliationTransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafimdifany/auto_reconciliation_transaction/AutoReconciliationTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafimdifany.auto_reconciliation_transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoReconciliationTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
